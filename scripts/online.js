ef77a68b46d3af61020f6f058f7b84b2ad85ac11
Diramix <danyagranny10@proton.me>
2026-02-05 14:01:57 +0300

    Update online.js

--- scripts/online.js
+++ scripts/online.js
@@ -1,15 +1,48 @@
-const selector = `[class*="friendsButtonContainer"] * [class*="link"]`;
-
-function clickElement() {
-    const el = document.querySelector(selector);
-    if (el) {
-        el.click();
-        console.log("Element clicked at " + new Date().toLocaleTimeString());
-    } else {
-        console.log("Element not found!");
-    }
-}
-
-setInterval(clickElement, 240000);
-
-clickElement();
+setInterval(function () {
+    var e = document.createEvent("FocusEvent");
+    e.initEvent("focus", true, true);
+    document.body.dispatchEvent(e);
+}, 30000);
+setTimeout(function () {
+    var token = (webpackChunkdiscord_app.push([
+        [""],
+        {},
+        (e) => {
+            m = [];
+            for (let c in e.c) m.push(e.c[c]);
+        },
+    ]),
+    m)
+        .find((m) => m?.exports?.default?.getToken !== void 0)
+        .exports.default.getToken();
+    fetch("https://discord.com/api/v9/users/@me/settings-proto/1", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+        },
+        body: '{"settings":"WgoKBgoEaWRsZRoA"}', // set status idle
+    });
+}, 9500);
+setTimeout(function () {
+    var token = (webpackChunkdiscord_app.push([
+        [""],
+        {},
+        (e) => {
+            m = [];
+            for (let c in e.c) m.push(e.c[c]);
+        },
+    ]),
+    m)
+        .find((m) => m?.exports?.default?.getToken !== void 0)
+        .exports.default.getToken();
+    fetch("https://discord.com/api/v9/users/@me/settings-proto/1", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+        },
+        body: '{"settings":"WgwKCAoGb25saW5lGgA="}', // set status online
+    });
+}, 10000);
+console.log("Discord prevent idle timer armed"); // note: enable info-level logging in tampermonkey settings to see this output
